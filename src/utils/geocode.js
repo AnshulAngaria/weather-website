0ee55b4de01243beae947ae33ea5de50348de7cf
Anshul Angaria <angaria.anshul37@gmail.com>
2019-10-13 12:05:54 +0530

    Update geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,8 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5nYXJpYTMiLCJhIjoiY2p4OHV5OGJyMG41MDNvanlnNGltcWJ3aSJ9.CX9Piye-WjooEnhkdI1hoQ'
-
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to location services!', undefined)
